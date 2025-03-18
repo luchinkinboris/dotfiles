1e05c762c2b2b12ade47a4f7b4a8c362d1745692
Borislav <luchinkinboris@mail.ru>
2025-03-18 20:04:33 +0300

    added blink

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -81,6 +81,8 @@ require('lazy').setup({
 
   require 'kickstart/plugins/obsidian',
 
+  require 'kickstart/plugins/blink',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
